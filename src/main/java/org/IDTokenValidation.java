9e48196328adc7e5544a92a369f37c8b0d303f6b
Isaac Jensen <isaac_jensen@mcafee.com>
2022-03-07 21:57:54 -0800

    removing confusing comment

--- src/main/java/org/IDTokenValidation.java
+++ src/main/java/org/IDTokenValidation.java
@@ -22,18 +22,6 @@ public class IDTokenValidation {
     public static final String id_token = "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJnb0dYUUxhRTA3cEFBXzBEMm9BLWZEbzBCVUtJLWRYZDByYVBpRlBUWnlJIn0.eyJleHAiOjE2NDY3MzYxNTAsImlhdCI6MTY0NjcwMDE1MCwiYXV0aF90aW1lIjowLCJqdGkiOiJmY2ExZTBmZS1hM2M4LTQyYWEtOTMwZS02ZGFiNzlhMDdhZTkiLCJpc3MiOiJodHRwczovL3VhbS5rZXljbG9hay5pYW1yYmFjLmNvbS9hdXRoL3JlYWxtcy9JQU0iLCJhdWQiOiJQb0NDbGllbnQiLCJzdWIiOiIxMDFkYWQyOS1mYWYyLTRkMWQtYjg3OS04MGE4ZWZkMGNhN2EiLCJ0eXAiOiJJRCIsImF6cCI6IlBvQ0NsaWVudCIsImF0X2hhc2giOiJFUWFpYkltNmZyZjR3b1h5WjdCeTRRIiwiYWNyIjoiMSIsImxvZ2luX2hpbnQiOiJzZXJ2aWNlLWFjY291bnQtcG9jY2xpZW50IiwiY2xpZW50SG9zdCI6IjE2MS42OS4yMy4yNCIsImNsaWVudElkIjoiUG9DQ2xpZW50IiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJpZHAiOiIwb2FlaTZiNGdrOGtlQTBrbzBoNyIsImdyb3VwcyI6W10sInByZWZlcnJlZF91c2VybmFtZSI6InNlcnZpY2UtYWNjb3VudC1wb2NjbGllbnQiLCJjbGllbnRBZGRyZXNzIjoiMTYxLjY5LjIzLjI0In0.QGrYIrG0AvMgw94SpSfrgxkCrqEf7XP10GBGu-0cXT3ksQdqPAzjU96C4t8kdvMyuGMsgPqjFbsBspQy3-BMFLio13St8yiqNs3NqTDDuHdshQqAd1Sv6oQ9LRgUMFV9L5ZEyoYKVGGl4OdCPeTFs92gEqc1QR1BzePmHj3eVWd5KVxgfAYbTIclTdTUrxS98wXChHf37QvwCMnIQpLMWSnl8DGr5g_7_dQn5lDCMYtH2WdsGC5rzvhb4xzXChZ8td89QlPWpm7eL7riDdyZSwLuflJiIentZZT0LCMWCxOv-XTAKwcK_RnBybtI7m-F6UIBzRFhKcCzJBkvtWjhaw";
     public static final String[] id_token_parts = id_token.split("\\.");
 
-    // Constants that come from the keys your token was signed with.
-    // Correct values can be found from using the "kid" value and looking up the "n
-    // (MODULUS)" and "e (EXPONENT)" fields
-    // at the following url: https://login.salesforce.com/id/keys
-    // MAJOR NOTE: This url will work for 90% of your use cases, but for the other
-    // 10%
-    // you'll need to make sure you get the "kid" value from the instance url that
-    // the api responses from Salesforce suggest for your token, as the kid values
-    // *will* be different.
-    // e.g. Some users would need to get their kid values from
-    // https://na44.salesforce.com/id/keys for example.
-    // The following 2 values are hard coded to work with the "kid=196" key values.
     public static final String EXPONENT = "AQAB";
     public static final String MODULUS = "iYMh26U1IX7EhBT3GN2yXJOcJWfglG0vX7J3Fv_HzdfIWWDrrBxXgQQElJEQvzJgDCLyRmUZcaGftyMs_e0DV7-XxRIXBjLAqVJfZu1ERV8hf_8XBJpkuZ1S6ehDMI04041Abt8au74E4Z4XfAS3UwhSo-LrnKm7i9zztQdzzzw0XMFLipyrBeu3oOn8j17TOStioPK-SwHtS7_JJ3jLXk-AWdGAb86TwjBH74u24XcVUZuLJSHHPogcBW4nolWA5PGNzFRlUVOlAjHxRcZ1VO9sR_ogLMkDpJl-AqQokmKo2HU4v5PmjbJULZjjhtmVO3LzZMi3v10ROXPvAmRYBw";
 
@@ -58,7 +46,7 @@ public class IDTokenValidation {
         Security.addProvider(new BouncyCastleFipsProvider());
         Security.addProvider(new BouncyCastleJsseProvider());
         dumpJwtInfo();
-        validateToken(true); // true for fips providers, false for regular Java
+        validateToken(false); // true for fips providers, false for regular Java
     }
 
     public static void dump(String data) {
@@ -72,9 +60,9 @@ public class IDTokenValidation {
 
     public static void validateToken(boolean fips) throws CertificateException, IOException, NoSuchProviderException {
         // TODO UNCOMMENT BELOW TO GENERATE PUBLIC KEY VIA x5c
-        PublicKey publicKey = getPublicKeyFromCert("MIIClTCCAX0CBgF+4mvcLzANBgkqhkiG9w0BAQsFADAOMQwwCgYDVQQDDANJQU0wHhcNMjIwMjEwMDY1NTI2WhcNMzIwMjEwMDY1NzA2WjAOMQwwCgYDVQQDDANJQU0wggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQCJgyHbpTUhfsSEFPcY3bJck5wlZ+CUbS9fsncW/8fN18hZYOusHFeBBASUkRC/MmAMIvJGZRlxoZ+3Iyz97QNXv5fFEhcGMsCpUl9m7URFXyF//xcEmmS5nVLp6EMwjTjTjUBu3xq7vgThnhd8BLdTCFKj4uucqbuL3PO1B3PPPDRcwUuKnKsF67eg6fyPXtM5K2Kg8r5LAe1Lv8kneMteT4BZ0YBvzpPCMEfvi7bhdxVRm4slIcc+iBwFbieiVYDk8Y3MVGVRU6UCMfFFxnVU72xH+iAsyQOkmX4CpCiSYqjYdTi/k+aNslQtmOOG2ZU7cvNkyLe/XRE5c+8CZFgHAgMBAAEwDQYJKoZIhvcNAQELBQADggEBADE5gy0UlfFj+5SuttQh48352HW4JYMDxcVa1lJufBcumpeqNSx7zA4vlLJoL74yjvdvEmLAMF0fzOPYm1GGbv4IwEx8AI7zBxvoMF5DU17E+TmDgDRdI9wIBCQrFXGgfQiDED/3ZDYpleAgimOw9gEW6L0dptSBj+QGOdHzflhJJKN1PZZjjeY+5/Lh3kqVGnQZtTP0qWip/ay4s8I40QRWOTIvne7W/YE1rkm7mDfYI3DIqHGNwsxk01pa9lKWVt+nosfo8pxBzepkat3NfiKHAReScfvI2Mt4FLE/0Sqe/VAuFUAJdEkYFrRiPGiLSIL2C2D6VzRWnYdu1+WtLrk=", fips);
+        //PublicKey publicKey = getPublicKeyFromCert("MIIClTCCAX0CBgF+4mvcLzANBgkqhkiG9w0BAQsFADAOMQwwCgYDVQQDDANJQU0wHhcNMjIwMjEwMDY1NTI2WhcNMzIwMjEwMDY1NzA2WjAOMQwwCgYDVQQDDANJQU0wggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQCJgyHbpTUhfsSEFPcY3bJck5wlZ+CUbS9fsncW/8fN18hZYOusHFeBBASUkRC/MmAMIvJGZRlxoZ+3Iyz97QNXv5fFEhcGMsCpUl9m7URFXyF//xcEmmS5nVLp6EMwjTjTjUBu3xq7vgThnhd8BLdTCFKj4uucqbuL3PO1B3PPPDRcwUuKnKsF67eg6fyPXtM5K2Kg8r5LAe1Lv8kneMteT4BZ0YBvzpPCMEfvi7bhdxVRm4slIcc+iBwFbieiVYDk8Y3MVGVRU6UCMfFFxnVU72xH+iAsyQOkmX4CpCiSYqjYdTi/k+aNslQtmOOG2ZU7cvNkyLe/XRE5c+8CZFgHAgMBAAEwDQYJKoZIhvcNAQELBQADggEBADE5gy0UlfFj+5SuttQh48352HW4JYMDxcVa1lJufBcumpeqNSx7zA4vlLJoL74yjvdvEmLAMF0fzOPYm1GGbv4IwEx8AI7zBxvoMF5DU17E+TmDgDRdI9wIBCQrFXGgfQiDED/3ZDYpleAgimOw9gEW6L0dptSBj+QGOdHzflhJJKN1PZZjjeY+5/Lh3kqVGnQZtTP0qWip/ay4s8I40QRWOTIvne7W/YE1rkm7mDfYI3DIqHGNwsxk01pa9lKWVt+nosfo8pxBzepkat3NfiKHAReScfvI2Mt4FLE/0Sqe/VAuFUAJdEkYFrRiPGiLSIL2C2D6VzRWnYdu1+WtLrk=", fips);
         // TODO UNCOMMMENT TO GENERATE PUBLIC KEY VIA MODULUS/EXPONENT
-        //PublicKey publicKey = getPublicKey(MODULUS, EXPONENT, fips);
+        PublicKey publicKey = getPublicKey(MODULUS, EXPONENT, fips);
 
         byte[] data = (id_token_parts[0] + "." + id_token_parts[1]).getBytes(StandardCharsets.UTF_8);
 
